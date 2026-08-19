22a3cbaf4ea16c202a2e4c2655be31c2fac916b0
Cndtnl_Cognition <storageunit199@gmail.com>
2026-08-19 16:55:44 +1000

    WHY DID THESE EXIST

--- src/main/java/com/cogworks/unorthodoxweapons/items/FirestarterItem.java
+++
@@ -1,9 +0,0 @@
-package com.cogworks.unorthodoxweapons.items;
-
-import net.minecraft.world.item.Item;
-
-public class FirestarterItem extends Item {
-    public FirestarterItem(Properties properties) {
-        super(properties);
-    }
-}
